b460da18845a2a456e8c5829a1efef14dc04239b
davidgfb <davidgracia99@gmail.com>
2020-10-25 12:12:03 +0100

    Update and rename hm.cpp to HMCC.cpp

---
+++ holamundo/HMCC.cpp
@@ -0,0 +1,7 @@
+//hola mundo con cabecera stdio.h HMCC este ejemplo usa la cabecera stdio.h para usar la funcion printf
+#include <stdio.h>
+
+int main(){
+    printf("hola mundo");
+    return 0;
+}
--- holamundo/hm.cpp
+++
@@ -1,7 +0,0 @@
-//este ejemplo usa la cabecera stdio para usar la funcion printf
-#include <stdio.h>
-
-int main(){
-    printf("hola mundo");
-    return 0;
-}
