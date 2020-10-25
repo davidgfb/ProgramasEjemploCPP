77741bf4ed71a80a41f153fb6f598dee1db6d0f7
davidgfb <davidgracia99@gmail.com>
2020-10-25 11:05:50 +0100

    Create Cadenas_01.cpp

---
+++ Cadenas_01.cpp
@@ -0,0 +1,17 @@
+//Cadenas_01 -----------------(página 52 presentación)-----------------------------------------------
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char Nombre[30];
+
+    cout << "Escriba su nombre completo: ";
+    cin >> Nombre;
+    cout << endl << "Su nombre completo es: " << Nombre;
+
+    return 0;
+}
