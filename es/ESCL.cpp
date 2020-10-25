0a9f4cbfa30e7b9188cf9a4c997306d5240ed05e
davidgfb <davidgracia99@gmail.com>
2020-10-25 14:03:08 +0100

    Create ESCL.cpp

---
+++ es/ESCL.cpp
@@ -0,0 +1,13 @@
+//entrada salida con libreria ESCL
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    int i = 0;
+    cin >> i;
+    //%d o i significa base decimal
+    printf("%d",i);
+    return 0;
+}
